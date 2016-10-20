65d70ddb911ea8b9ed8b48f1caa2919c97478544
António Pereira <ape@domain.outsystems.com>
2016-10-20 09:42:02 +0100

    First Exercise: Explicit types

--- app.ts
+++ app.ts
@@ -1,2 +1,20 @@
-let x: number = 27;
-x = 1;
+type BankAccount = {money: number, deposit(value: number): void};
+ 
+ let bankAccount: BankAccount = {
+    money: 2000,
+    deposit(value: number): void {
+        this.money += value;
+    }
+};
+
+type PersonnalAccount = {name: string, bankAccount: BankAccount, hobbies: string[]};
+
+let myself: PersonnalAccount = {
+    name: "Max",
+    bankAccount: bankAccount,
+    hobbies: ["Sports", "Cooking"]
+};
+
+myself.bankAccount.deposit(3000);
+
+console.log(myself);
