d3a21a3d9cd560129107cbc60c48d9d6aff11fe6
António Pereira <ape@domain.outsystems.com>
2016-10-21 10:04:51 +0100

    Section 4 - Exercise

--- app.ts
+++ app.ts
@@ -1,20 +1,33 @@
-type BankAccount = {money: number, deposit(value: number): void};
- 
- let bankAccount: BankAccount = {
-    money: 2000,
-    deposit(value: number): void {
-        this.money += value;
-    }
-};
+    // Exercise 1 - Maybe use an Arrow Function?
+    const double = (value: number): number => value * 2;
+    console.log(double(10));
+     
+    // Exercise 2 - If only we could provide some default values...
+    /*var greet = function (name = "Max") {
+        console.log("Hello, " + name);
+    };*/
+    const greet = (name: string = "Max") => console.log(`Hello ${name}`);
+    greet();
+    greet("Anna");
+     
+    // Exercise 3 - Isn't there a shorter way to get all these Values?
+    var numbers = [-3, 33, 38, 5];
+    console.log(Math.min(...numbers));
+     
+    // Exercise 4 - I have to think about Exercise 3 ...
+    var newArray = [...numbers, 55, 20];
+    //Array.prototype.push.apply(newArray, numbers);
+    console.log(newArray);
+     
+    // Exercise 5 - That's a well-constructed array.
+    var testResults = [3.89, 2.99, 1.38];
+    
+    const [result1, result2, result3] = testResults;
+    console.log(result1, result2, result3);
+     
+    // Exercise 6 - And a well-constructed object!
+    var scientist = {firstName: "Will", experience: 12};
 
-type PersonnalAccount = {name: string, bankAccount: BankAccount, hobbies: string[]};
+    const {firstName, experience} = scientist;
 
-let myself: PersonnalAccount = {
-    name: "Max",
-    bankAccount: bankAccount,
-    hobbies: ["Sports", "Cooking"]
-};
-
-myself.bankAccount.deposit(3000);
-
-console.log(myself);
+    console.log(firstName, experience);
